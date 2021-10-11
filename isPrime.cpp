379c2671d339e0761fb515924a23a26b3fdc3af8
rohithpala <63358944+rohithpala@users.noreply.github.com>
2021-10-11 17:15:03 +0530

    C++ Programs

---
+++ isPrime.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+bool isPrime(int n) {
+     int i, square_root = (int) sqrt(n);
+     for(i = 2 ; i < square_root; i++) {
+          if(isPrime(i) && n % i == 0)
+               return false;
+     }
+     return true;
+}
+
+int main() {
+     int n;
+     cout << "Enter a Number: ";
+     cin >> n;
+     if(isPrime(n)) {
+          cout << n << " is a Prime Number";
+     } else {
+          cout << n << " is not a Prime Number";
+     }
+     return 0;
+}
